927fd361a2acfd89d9fa1203073439423d4b1987
ofer <ofer.kafri@zooz.com>
2017-11-13 21:56:15 +0200

    refactor

--- src/app/show-list/show-video/show-video.component.ts
+++ src/app/show-list/show-video/show-video.component.ts
@@ -7,7 +7,7 @@ import { Component, EventEmitter, Input, Output } from "@angular/core";
 })
 export class ShowVideoComponent {
 
-    @Input() show: any;
+    @Input() video: any;
 
     constructor() {
 
